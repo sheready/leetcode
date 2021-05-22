52f679bd1d3ef7d0b680b5ce78b764f497ea85f6
sheready <csstlw@gmail.com>
2021-05-22 23:37:31 +0300

    Time: 84 ms (28.27%), Space: 38.5 MB (84.99%) - LeetHub

---
+++ remove-element/remove-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+  var len = nums.length;
+  var count = 0;
+  for (var i = 0; i < len; i++) {
+    if (nums[i] !== val) nums[count++] = nums[i];
+  }
+  return count;
+    
+};
